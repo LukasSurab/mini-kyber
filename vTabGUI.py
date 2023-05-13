9d9725f3b921e1a5f9d6dc8e00991eb1df7b576b
LukasSurab <99997490+LukasSurab@users.noreply.github.com>
2023-05-14 00:10:39 +0200

    Update vTabGUI.py

--- vTabGUI.py
+++ vTabGUI.py
@@ -4,6 +4,7 @@ from tkinter import ttk
 from tktooltip import ToolTip
 from PIL import Image, ImageTk
 import tkinter.font as font
+from helperFunctions import *
 
 class FrameVTab:
     def __init__(self, notebook):
@@ -50,43 +51,43 @@ class FrameVTab:
         self.rV = v['r']
 
         self.rButtonHover = tk.Button(self.frame,text = "r",state='disabled',background='lightgreen')
-        ToolTip(self.rButtonHover,msg=str(self.rV))
+        ToolTip(self.rButtonHover,msg=str(smallRandomXbarToX(self.rV)))
         self.rButtonHover.place(x = 248,y = 31,width=175, height=48)
         self.rButtonHover['font'] = self.myFont
         self.rButtonHover2 = tk.Button(self.frame,text = "r",state='disabled',background='lightgreen')
-        ToolTip(self.rButtonHover2,msg=str(self.rV))
+        ToolTip(self.rButtonHover2,msg=str(smallRandomXbarToX(self.rV)))
         self.rButtonHover2.place(x = 248,y = 246,width=175, height=48)
         self.rButtonHover2['font'] = self.myFont
         self.rButtonHover3 = tk.Button(self.frame,text = "r",state='disabled',background='lightgreen')
-        ToolTip(self.rButtonHover3,msg=str(self.rV))
+        ToolTip(self.rButtonHover3,msg=str(smallRandomXbarToX(self.rV)))
         self.rButtonHover3.place(x = 248,y = 460,width=175, height=48)
         self.rButtonHover3['font'] = self.myFont
         self.tButtonHover = tk.Button(self.frame,text = "t",state='disabled',background='lightblue')
-        ToolTip(self.tButtonHover,msg=str(self.tV))
+        ToolTip(self.tButtonHover,msg=str(smallRandomXbarToX(self.tV)))
         self.tButtonHover.place(x = 430,y = 31,width=48, height=175)
         self.tButtonHover['font'] = self.myFont
         self.e2ButtonHover = tk.Button(self.frame,text = "e2",state='disabled',background='yellow')
-        ToolTip(self.e2ButtonHover,msg=str(self.e2V))
+        ToolTip(self.e2ButtonHover,msg=str(msgXbarToX(self.e2V)))
         self.e2ButtonHover.place(x = 522,y = 94,width=48, height=48)
         self.e2ButtonHover['font'] = self.myFont
         self.e2ButtonHover2 = tk.Button(self.frame,text = "e2",state='disabled',background='yellow')
-        ToolTip(self.e2ButtonHover2,msg=str(self.e2V))
+        ToolTip(self.e2ButtonHover2,msg=str(msgXbarToX(self.e2V)))
         self.e2ButtonHover2.place(x = 817,y = 309,width=48, height=48)
         self.e2ButtonHover2['font'] = self.myFont
         self.mButtonHover = tk.Button(self.frame,text = "m",state='disabled',background='lightgrey')
-        ToolTip(self.mButtonHover,msg=str(self.mV))
+        ToolTip(self.mButtonHover,msg=str(msgXbarToX(self.mV)))
         self.mButtonHover.place(x = 611,y = 94,width=48, height=48)
         self.mButtonHover['font'] = self.myFont
         self.mButtonHover2 = tk.Button(self.frame,text = "m",state='disabled',background='lightgrey')
-        ToolTip(self.mButtonHover2,msg=str(self.mV))
+        ToolTip(self.mButtonHover2,msg=str(msgXbarToX(self.mV)))
         self.mButtonHover2.place(x = 908,y = 309,width=48, height=48)
         self.mButtonHover2['font'] = self.myFont
         self.mButtonHover3 = tk.Button(self.frame,text = "m",state='disabled',background='lightgrey')
-        ToolTip(self.mButtonHover3,msg=str(self.mV))
+        ToolTip(self.mButtonHover3,msg=str(msgXbarToX(self.mV)))
         self.mButtonHover3.place(x = 817,y = 523,width=48, height=48)
         self.mButtonHover3['font'] = self.myFont
         self.vButton = tk.Button(self.frame,text = "v",state='disabled',background='red')
-        ToolTip(self.vButton,msg=str(self.v))
+        ToolTip(self.vButton,msg=str(msgXbarToX(self.v)))
         self.vButton.place(x = 145,y = 94,width=48, height=48)
         self.vButton['font'] = self.myFont
         self.loadButtonv['state'] = 'disabled'
@@ -100,15 +101,15 @@ class FrameVTab:
         self.A = t['A']
         self.eV = t['e']
         self.AButtonHover = tk.Button(self.frame,text = "A",state='disabled',background='lightblue')
-        ToolTip(self.AButtonHover,msg=str(self.A))
+        ToolTip(self.AButtonHover,msg=str(matrixXbartoX(self.A)))
         self.AButtonHover.place(x = 440,y = 246,width=175, height=175)
         self.AButtonHover['font'] = self.myFont
         self.AButtonHover2 = tk.Button(self.frame,text = "A",state='disabled',background='lightblue')
-        ToolTip(self.AButtonHover2,msg=str(self.A))
+        ToolTip(self.AButtonHover2,msg=str(matrixXbartoX(self.A)))
         self.AButtonHover2.place(x = 440,y = 460,width=175, height=175)
         self.AButtonHover2['font'] = self.myFont
         self.eButtonHover = tk.Button(self.frame,text = "e",state='disabled',background='yellow')
-        ToolTip(self.eButtonHover,msg=str(self.eV))
+        ToolTip(self.eButtonHover,msg=str(smallRandomXbarToX(self.eV)))
         self.eButtonHover.place(x = 710,y = 246,width=48, height=175)
         self.eButtonHover['font'] = self.myFont
         self.loadButtonAE['state'] = 'disabled'
@@ -121,11 +122,11 @@ class FrameVTab:
             s = pickle.load(s_in)
         self.s = s['s']
         self.sButton = tk.Button(self.frame,text = "s",state='disabled',background='lightgreen')
-        ToolTip(self.sButton,msg=str(self.s))
+        ToolTip(self.sButton,msg=str(smallRandomXbarToX(self.s)))
         self.sButton.place(x = 625,y = 460,width=48, height=175)
         self.sButton['font'] = self.myFont
         self.sButton2 = tk.Button(self.frame,text = "s",state='disabled',background='lightgreen')
-        ToolTip(self.sButton2,msg=str(self.s))
+        ToolTip(self.sButton2,msg=str(smallRandomXbarToX(self.s)))
         self.sButton2.place(x = 625,y = 245,width=48, height=175)
         self.sButton2['font'] = self.myFont
         self.loadButtonsk['state'] = 'disabled'
